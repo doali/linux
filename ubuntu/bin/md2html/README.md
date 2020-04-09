9f133154f9880f8cb6888d7d3aa7e78eebaccda1
xavL <xou.xavl@gmail.com>
2020-04-09 12:28:36 +0200

    Script to generate HTML from Markdown

---
+++ ubuntu/bin/md2html/README.md
@@ -0,0 +1,27 @@
+# Convert Markdown to HTML
+
+# Requirements
+```bash
+sudo apt install pandoc
+```
+
+# Description
+Convert a file written in Markdown to HTML
+- use pandoc
+- css file
+
+# What is done
+Generate
+- an HTML file from ist Markdown (both have the same name)
+- along side with its md.css file
+- both located in output path
+
+# Usage
+```bash
+Usage: md2html <path_markdown_file> [<path_output>]
+```
+
+# Biblio
+- [css](https://raw.githubusercontent.com/KrauseFx/markdown-to-html-github-style/master/style.css)
+- [pandoc](https://pandoc.org/)
+- [script directory](https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script)
---
+++ ubuntu/bin/md2html/md2html.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+#set -x
+
+# ------------------------------------------------------------------------------
+# Convert a file written in Markdown to HTML
+# - use pandoc
+# - css file
+# CSS used comes from 
+# https://raw.githubusercontent.com/KrauseFx/markdown-to-html-github-style/master/style.css
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# return script path from the current working directory
+# https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir() {
+	local source="${BASH_SOURCE[0]}"
+	local dir=""
+	# While $source is a symlink, resolve it
+	while [ -h "$source" ]; do
+		dir="$(cd -P "$(dirname "$source")" && pwd)"
+		source="$(readlink "$source")"
+		# If $source was a relative symlink (so no "/" as prefix,
+		# need to resolve it relative to the symlink base directory
+		[[ $source != /* ]] && source="$dir/$source"
+	done
+	dir="$(cd -P "$(dirname "$source")" && pwd)"
+  echo $dir # returns to sdout (clever mean to return results)
+}
+
+# symlinks are not handled using as described in the following command
+#readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# ------------------------------------------------------------------------------
+readonly FILE_CSS=md.css
+readonly DIR_SCRIPT=$(get_script_dir) # gets from stdout
+# ------------------------------------------------------------------------------
+
+error_path() {
+	echo "Error path"
+	exit 1
+}
+
+# Generate
+# - an HTML file from ist Markdown (both have the same name)
+# - along side with its md.css file
+# - both located in $path_output
+gen() {
+	local path_md=${1}
+	local path_output=${2}
+
+	[ -f ${path_md} ] && [ -d ${path_output} ] || error_path
+
+	local file_name_md=$(basename ${path_md})
+
+	file_name_md_no_extension=${file_name_md%.*}
+
+	[ ! -f ${path_output}/${FILE_CSS} ] && cp -f ${DIR_SCRIPT}/${FILE_CSS} ${path_output}
+
+	$(which pandoc) -f markdown -t html ${path_md} --css ${FILE_CSS} >${path_output}/${file_name_md_no_extension}.html
+}
+
+usage() {
+	echo "Usage: $(basename ${0}) <path_markdown_file> [<path_output>]"
+}
+
+# ------------------------------------------------------------------------------
+if [ ${#} -eq 2 ]; then
+	gen ${1} ${2}
+elif [ ${#} -eq 1 ]; then
+	gen ${1} $(pwd)
+else
+	usage
+fi
+
+exit 0
