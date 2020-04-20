641cdeee2f7025ed4db85178c2c5c6fa6a0d38a1
doali <xou.xavl@gmail.com>
2020-04-20 11:36:55 +0200

    Correction interpollation

--- ubuntu/etc/install-tilix.sh
+++ ubuntu/etc/install-tilix.sh
@@ -6,7 +6,12 @@ OUTPUT_FILE=$HOME/.bashrc
 # tilix package
 sudo apt install tilix
 
-# Settings bashrc
+# 1 Settings bashrc
+echo "" >>$OUTPUT_FILE
+echo "# https://gnunn1.github.io/tilix-web/manual/vteconfig/" >>$OUTPUT_FILE
+echo "# Settings for tilix" >>$OUTPUT_FILE
+
+# 2 Settings bashrc
 cat << EOT >>$OUTPUT_FILE
 if [ \$TILIX_ID ] || [ \$VTE_VERSION ]; then
         source /etc/profile.d/vte.sh
@@ -15,12 +20,13 @@ EOT
 
 sudo ln -snf /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
-# Settings bashrc
+# 3 Settings bashrc
+echo "" >>$OUTPUT_FILE
 cat << EOT >>$OUTPUT_FILE
 function custom_prompt() {
   __git_ps1 "\[\033[0;31m\]\u \[\033[0;36m\]\h:\w\[\033[00m\]" " \n\[\033[0;31m\]>\[\033[00m\] " " %s"
-  VTE_PWD_THING="$(__vte_osc7)"
-  PS1="$PS1$VTE_PWD_THING"
+  VTE_PWD_THING="\$(__vte_osc7)"
+  PS1="\$PS1\$VTE_PWD_THING"
 }
 PROMPT_COMMAND=custom_prompt
 EOT
