430ff0023f94e7df5ae4f4d7a1ac636824f5ed3c
doali <xou.xavl@gmail.com>
2020-04-22 15:42:49 +0200

    option --view added - enable to open html (generated) in firefox

--- ubuntu/bin/md2html/md2html.sh
+++ ubuntu/bin/md2html/md2html.sh
@@ -14,18 +14,18 @@
 # return script path from the current working directory
 # https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 get_script_dir() {
-	local source="${BASH_SOURCE[0]}"
-	local dir=""
-	# While $source is a symlink, resolve it
-	while [ -h "$source" ]; do
-		dir="$(cd -P "$(dirname "$source")" && pwd)"
-		source="$(readlink "$source")"
-		# If $source was a relative symlink (so no "/" as prefix,
-		# need to resolve it relative to the symlink base directory
-		[[ $source != /* ]] && source="$dir/$source"
-	done
-	dir="$(cd -P "$(dirname "$source")" && pwd)"
-	echo $dir # returns to sdout (clever mean to return results)
+  local source="${BASH_SOURCE[0]}"
+  local dir=""
+  # While $source is a symlink, resolve it
+  while [ -h "$source" ]; do
+    dir="$(cd -P "$(dirname "$source")" && pwd)"
+    source="$(readlink "$source")"
+    # If $source was a relative symlink (so no "/" as prefix,
+    # need to resolve it relative to the symlink base directory
+    [[ $source != /* ]] && source="$dir/$source"
+  done
+  dir="$(cd -P "$(dirname "$source")" && pwd)"
+  echo $dir # returns to sdout (clever mean to return results)
 }
 
 # symlinks are not handled using as described in the following command
@@ -35,17 +35,18 @@ get_script_dir() {
 readonly FILE_CSS=md.css
 readonly DIR_SCRIPT=$(get_script_dir) # gets from stdout
 readonly PANDOC=$(which pandoc)
+readonly BROWSER=$(which firefox)
 # ------------------------------------------------------------------------------
 
 error_path() {
-	echo "Error path"
-	exit 1
+  echo "Error path"
+  exit 1
 }
 
 error_cmd() {
-	echo "Error pandoc not found"
-	echo "Install it with : sudo apt install pandoc"
-	exit 1
+  echo "Error pandoc not found"
+  echo "Install pandoc with : sudo apt install pandoc"
+  exit 1
 }
 
 # Generate
@@ -53,51 +54,68 @@ error_cmd() {
 # - along side with its md.css file
 # - both located in $path_output
 gen() {
-	local path_md=${1}
-	local path_output=${2}
+  local path_md=${1}
+  local path_output=${2}
 
-	[ -f ${path_md} ] && [ -d ${path_output} ] || error_path
+  [ -f ${path_md} ] && [ -d ${path_output} ] || error_path
 
-	local file_name_md=$(basename ${path_md})
+  local file_name_md=$(basename ${path_md})
 
-	file_name_md_no_extension=${file_name_md%.*}
+  file_name_md_no_extension=${file_name_md%.*}
 
-	[ ! -f ${path_output}/${FILE_CSS} ] && cp -f ${DIR_SCRIPT}/${FILE_CSS} ${path_output}
+  [ ! -f ${path_output}/${FILE_CSS} ] && cp -f ${DIR_SCRIPT}/${FILE_CSS} ${path_output}
 
-	${PANDOC} -f markdown -t html ${path_md} --css ${FILE_CSS} >${path_output}/${file_name_md_no_extension}.html
+  ${PANDOC} -f markdown -t html ${path_md} --css ${FILE_CSS} > ${path_output}/${file_name_md_no_extension}.html
 }
 
 # Delete all files ending by .html or .css
 clean() {
-  local exist_html=$(ls *.html 2>/dev/null | wc -l)
+  local exist_html=$(ls *.html 2> /dev/null | wc -l)
   [ ${exist_html} -gt 0 ] && rm -i *.html
-	[ -f md.css ] && rm md.css
+  [ -f md.css ] && rm md.css
+}
+
+# View html file
+view() {
+  local file=$1
+  [ -f $file ] && ${BROWSER} --new-tab ${file} || error_path
 }
 
 usage() {
-	echo "Usage:"
-	echo -e "\t$(basename ${0}) <path_markdown_file> [<path_output>]"
-	echo -e "\t$(basename ${0}) --clean"
+  echo "Usage:"
+  echo -e "\t$(basename ${0}) <path_markdown_file> [<path_output>]"
+  echo -e "\t$(basename ${0}) --view <path_html>"
+  echo -e "\t$(basename ${0}) --clean"
 }
 
 # ------------------------------------------------------------------------------
 if [ -z ${PANDOC} ]; then
-	error_cmd
+  error_cmd
 fi
 
 if [ ${#} -eq 2 ]; then
-	gen ${1} ${2}
+  case "${1}" in
+    --view)
+      [ -z ${2} ] && usage || view ${2}
+      ;;
+    *)
+      gen ${1} ${2}
+      ;;
+  esac
 elif [ ${#} -eq 1 ]; then
-	case "$1" in
-	--clean)
-		clean
-		;;
-	*)
-		gen ${1} $(pwd)
-		;;
-	esac
+  case "$1" in
+    --clean)
+      clean
+      ;;
+    --view)
+      usage
+      ;;
+    *)
+      gen ${1} $(pwd)
+      ;;
+  esac
 else
-	usage
+  usage
 fi
 
 exit 0
