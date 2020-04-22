01097f6d7856a32c9077c08a40820021acb2d757
doali <xou.xavl@gmail.com>
2020-04-22 21:57:57 +0200

    Installation version plus récente que celle sur le dépot ubuntu

---
+++ ubuntu/etc/install-ansible.sh
@@ -0,0 +1,5 @@
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
