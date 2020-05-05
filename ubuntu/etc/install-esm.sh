7b7259fd253dd2949d45083b58d89afa7f969dcf
doali <xou.xavl@gmail.com>
2020-05-05 15:53:04 +0200

    Another installation

--- ubuntu/etc/install-esm.sh
+++ ubuntu/etc/install-esm.sh
@@ -2,3 +2,4 @@
 # https://stackoverflow.com/questions/39436322/node-js-syntaxerror-unexpected-token-import/58542987#58542987
 # Node
 sudo npm install -g --save esm
+#sudo npm install -g esm-node esm
