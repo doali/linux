ca2eb8d058f9dc0eb81f92354b8439bb984f6a65
XLJ <xavier-externe.lejus@edf.fr>
2024-02-08 17:15:43 +0100

    Generation d'un .pdf depuis un .md - utilise le script md2html

--- ubuntu/bin/md2html/md2pdf.sh
+++ ubuntu/bin/md2html/md2pdf.sh
@@ -13,7 +13,7 @@ readonly FILE_HTML="${FILE_MD%.md}".html
 readonly FILE_PDF="${FILE_MD%.md}".pdf
 readonly FILE_CSS=md.css
 
-readonly MD2HTML_BIN=~/.local/bin/md2html.sh
+readonly MD2HTML_BIN=~/.local/bin/md2html
 readonly CONV_HTML2PDF=wkhtmltopdf
 
 # Conversion de md -> html
@@ -24,5 +24,5 @@ ${CONV_HTML2PDF} ${FILE_HTML} ${FILE_PDF}
 [ ${?} -ne 0 ] && echo "KO Conversion de ${FILE_HTML} en ${FILE_HTML}"
 
 # Nettoyage
-[ -e ${FILE_CSS} ] && rm ${FILE_CSS} && "OK suppression de ${FILE_CSS}"
-[ -e ${FILE_HTML} ] && rm ${FILE_HTML} && "OK suppression de ${FILE_HTML}"
+[ -e ${FILE_CSS} ] && rm ${FILE_CSS} && echo "OK suppression de ${FILE_CSS}"
+[ -e ${FILE_HTML} ] && rm ${FILE_HTML} && echo "OK suppression de ${FILE_HTML}"
