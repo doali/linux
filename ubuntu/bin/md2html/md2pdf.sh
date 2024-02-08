ace72e5f19ceeb7bae39017ab4460f0bb8493fe3
XLJ <xavier-externe.lejus@edf.fr>
2024-02-08 09:42:18 +0100

    Suite au fait que le plugin MD->PDF de VSCode est KO... - reutilisation du script md2html - ajout de ce script (car pandoc ne fonctionne pas sur CentOS 7...)

---
+++ ubuntu/bin/md2html/md2pdf.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# ------------------------------------------------------------------------------
+# Conversion d'un fichier .md en un fichier .pdf
+# - utilisation du script perso md2html
+# - utilisation de wkhtmltopdf
+# ------------------------------------------------------------------------------
+# jeu. févr.  8 09:37:09 CET 2024
+# ------------------------------------------------------------------------------
+
+readonly FILE_MD=$1
+readonly FILE_HTML="${FILE_MD%.md}".html
+readonly FILE_PDF="${FILE_MD%.md}".pdf
+readonly FILE_CSS=md.css
+
+readonly MD2HTML_BIN=~/.local/bin/md2html.sh
+readonly CONV_HTML2PDF=wkhtmltopdf
+
+# Conversion de md -> html
+${MD2HTML_BIN} ${FILE_MD}
+[ ${?} -ne 0 ] && echo "KO Conversion de ${FILE_MD} en ${FILE_HTML}"
+
+${CONV_HTML2PDF} ${FILE_HTML} ${FILE_PDF}
+[ ${?} -ne 0 ] && echo "KO Conversion de ${FILE_HTML} en ${FILE_HTML}"
+
+# Nettoyage
+[ -e ${FILE_CSS} ] && rm ${FILE_CSS} && "OK suppression de ${FILE_CSS}"
+[ -e ${FILE_HTML} ] && rm ${FILE_HTML} && "OK suppression de ${FILE_HTML}"
